9b6ccda766e6fff112d0f8c9eaf2ce0089795b0b
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-08-13 10:55:26 +0530

    [SYS_FS] Regenerated demo for fs_sdspi_fat_pic32cm_le00_curiosity_pro
    MH3-55600
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/fs/sdspi_fat/firmware/src/config/pic32cm_le00_curiosity_pro/initialization.c
+++ apps/fs/sdspi_fat/firmware/src/config/pic32cm_le00_curiosity_pro/initialization.c
@@ -97,7 +97,7 @@ const DRV_SDSPI_PLIB_INTERFACE drvSDSPI0PlibAPI = {
     .read = (DRV_SDSPI_PLIB_READ)SERCOM3_SPI_Read,
 
     /* SPI PLIB Transfer Status function */
-    .isBusy = (DRV_SDSPI_PLIB_IS_BUSY)SERCOM3_SPI_IsBusy,
+    .isTransmitterBusy = (DRV_SPI_PLIB_TRANSMITTER_IS_BUSY)SERCOM3_SPI_IsTransmitterBusy,
 
     .transferSetup = (DRV_SDSPI_PLIB_SETUP)SERCOM3_SPI_TransferSetup,
 
@@ -221,13 +221,15 @@ const SYS_FS_FUNCTIONS FatFsFunctions =
 };
 
 
+
 const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
 {
     {
         .nativeFileSystemType = FAT,
         .nativeFileSystemFunctions = &FatFsFunctions
-    }
+    },
 };
+
 
 // </editor-fold>
 
--- apps/fs/sdspi_fat/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
+++ apps/fs/sdspi_fat/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
@@ -77,6 +77,7 @@ void PORT_Initialize(void)
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x1;
    PORT_REGS->GROUP[1].PORT_OUT = 0x1;
+   PORT_REGS->GROUP[1].PORT_PINCFG[0] = 0x0;
    PORT_REGS->GROUP[1].PORT_PINCFG[1] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[2] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[3] = 0x1;
@@ -87,6 +88,7 @@ void PORT_Initialize(void)
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x80000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x180000;
+   PORT_REGS->GROUP[2].PORT_PINCFG[19] = 0x0;
    PORT_REGS->GROUP[2].PORT_PINCFG[20] = 0x6;
    PORT_REGS->GROUP[2].PORT_PINCFG[28] = 0x6;
 
@@ -302,7 +304,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
     PORT_GROUP group = GET_PORT_GROUP(pin);
     uint32_t pin_num = ((uint32_t)pin) & 0x1FU;
     uint32_t pinmux_val = (uint32_t)((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)];
-    
+
     /* For odd pins */
     if (0U != (pin_num & 0x01U))
     {
@@ -313,7 +315,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
         pinmux_val = (pinmux_val & ~0x0FU) | periph_func;
     }
     ((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)] = (uint8_t)pinmux_val;
-    
+
     /* Enable peripheral control of the pin */
     ((port_group_registers_t*)group)->PORT_PINCFG[pin_num] |= (uint8_t)PORT_PINCFG_PMUXEN_Msk;
 }
