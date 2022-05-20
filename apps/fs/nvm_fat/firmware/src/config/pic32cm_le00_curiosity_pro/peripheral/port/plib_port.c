695661f9f410f527717c40b9757147f28831a57e
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-20 10:20:25 +0530

    [SYS_FS] Regenerated demo for fs_nvm_fat_pic32cm_le00_curiosity_pro
    MH3-64472
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/fs/nvm_fat/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
+++ apps/fs/nvm_fat/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -83,9 +82,11 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[2].PORT_PINCFG[20] = 0x6;
    PORT_REGS->GROUP[2].PORT_PINCFG[28] = 0x6;
 
+   PORT_REGS->GROUP[2].PORT_PMUX[10] = 0x0;
+   PORT_REGS->GROUP[2].PORT_PMUX[14] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
