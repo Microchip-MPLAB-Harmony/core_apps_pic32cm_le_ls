6075bccffd5c3b7f2a7bc9662a5106097153fa18
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-11-12 12:52:04 +0530

    [RTOS] Regenerated demo for rtos/freertos/basic_freertosfirmware_pic32cm_le00_curiosity_pro.X
    MH3-102874

--- apps/rtos/freertos/basic_freertos/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/eic/plib_eic.c
+++ apps/rtos/freertos/basic_freertos/firmware/src/config/pic32cm_le00_curiosity_pro/peripheral/eic/plib_eic.c
@@ -81,6 +81,7 @@ void EIC_Initialize (void)
 
     /* NMI Control register */
 
+
     /* Interrupt sense type and filter control for EXTINT channels 0 to (16-1) */
     EIC_REGS->EIC_CONFIG0 =  EIC_CONFIG0_SENSE0_NONE 
         | EIC_CONFIG0_SENSE1_NONE 
@@ -99,6 +100,7 @@ void EIC_Initialize (void)
         | EIC_CONFIG1_SENSE13_NONE 
         | EIC_CONFIG1_SENSE14_NONE 
         | EIC_CONFIG1_SENSE15_NONE ;
+
 
     /* External Interrupt Asynchronous Mode enable */
     EIC_REGS->EIC_ASYNCH = 0x1000;
