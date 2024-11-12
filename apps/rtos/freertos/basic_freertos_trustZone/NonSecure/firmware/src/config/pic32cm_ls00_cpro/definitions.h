7fb9b234cffc059d22f4f57e678645cddb2403ba
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-11-12 14:12:41 +0530

    [RTOS] Regenerated demo for rtos_freertos_basic_freertos_trustZone_pic32cm_ls00_cpro
    MH3-102876

--- apps/rtos/freertos/basic_freertos_trustZone/NonSecure/firmware/src/config/pic32cm_ls00_cpro/definitions.h
+++ apps/rtos/freertos/basic_freertos_trustZone/NonSecure/firmware/src/config/pic32cm_ls00_cpro/definitions.h
@@ -69,13 +69,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CM5164LS00100"
-#define DEVICE_ARCH			 "CORTEX-M23"
-#define DEVICE_FAMILY		 "PIC32CM"
-#define DEVICE_SERIES		 "PIC32CMLS00"
+#define DEVICE_NAME          "PIC32CM5164LS00100"
+#define DEVICE_ARCH          "CORTEX-M23"
+#define DEVICE_FAMILY        "PIC32CM"
+#define DEVICE_SERIES        "PIC32CMLS00"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
