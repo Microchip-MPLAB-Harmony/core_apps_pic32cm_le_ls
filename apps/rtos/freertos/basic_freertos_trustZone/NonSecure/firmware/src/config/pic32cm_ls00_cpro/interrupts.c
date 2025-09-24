c7b9183ef97123995b9d0bb066cdc322879b578d
Syam Nair <Syam.Nair@microchip.com>
2025-09-24 11:21:25 +0530

    [APP] Regenerated demo for rtos__freertos__basic_freertos__firmware__pic32cm_le00_curiosity_pro.X
    MH3C-320

--- apps/rtos/freertos/basic_freertos_trustZone/NonSecure/firmware/src/config/pic32cm_ls00_cpro/interrupts.c
+++ apps/rtos/freertos/basic_freertos_trustZone/NonSecure/firmware/src/config/pic32cm_ls00_cpro/interrupts.c
@@ -20,7 +20,7 @@
 
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -83,7 +83,7 @@ void __attribute__((optimize("-O1"), long_call, noreturn, used))Dummy_Handler(vo
 /* MISRAC 2012 deviation block start */
 /* MISRA C-2012 Rule 8.6 deviated 1 times.  Deviation record ID -  H3_MISRAC_2012_R_8_6_DR_1 */
 /* Device vectors list dummy definition*/
-extern void xPortSysTickHandler        ( void ) __attribute__((weak, alias("Dummy_Handler")));
+extern void xPortSysTickHandler        ( void ) __attribute__((weak, alias("Dummy_Handler"),noreturn));
 
 
 /* MISRAC 2012 deviation block end */
